966050c9d75f2662a0033ebbe8154b0e81e66c31
narjoDev <narjoDev@users.noreply.github.com>
2023-11-19 23:00:11 -0800

    Implement GDScript basics

---
+++ lang/gdscript/gdscript.py
@@ -0,0 +1,210 @@
+from talon import Context, actions, settings
+
+ctx = Context()
+ctx.matches = r"""
+code.language: gdscript
+"""
+
+
+# WARNING: Commented blocks may still be python implementation
+@ctx.action_class("user")
+class UserActions:
+    # def code_operator_lambda():
+    #     actions.user.insert_between("lambda ", ": ")
+
+    def code_operator_subscript():
+        actions.user.insert_between("[", "]")
+
+    def code_operator_assignment():
+        actions.auto_insert(" = ")
+
+    def code_operator_subtraction():
+        actions.auto_insert(" - ")
+
+    def code_operator_subtraction_assignment():
+        actions.auto_insert(" -= ")
+
+    def code_operator_addition():
+        actions.auto_insert(" + ")
+
+    def code_operator_addition_assignment():
+        actions.auto_insert(" += ")
+
+    def code_operator_multiplication():
+        actions.auto_insert(" * ")
+
+    def code_operator_multiplication_assignment():
+        actions.auto_insert(" *= ")
+
+    def code_operator_exponent():
+        actions.auto_insert(" ** ")
+
+    def code_operator_division():
+        actions.auto_insert(" / ")
+
+    def code_operator_division_assignment():
+        actions.auto_insert(" /= ")
+
+    def code_operator_modulo():
+        actions.auto_insert(" % ")
+
+    def code_operator_modulo_assignment():
+        actions.auto_insert(" %= ")
+
+    def code_operator_equal():
+        actions.auto_insert(" == ")
+
+    def code_operator_not_equal():
+        actions.auto_insert(" != ")
+
+    def code_operator_greater_than():
+        actions.auto_insert(" > ")
+
+    def code_operator_greater_than_or_equal_to():
+        actions.auto_insert(" >= ")
+
+    def code_operator_less_than():
+        actions.auto_insert(" < ")
+
+    def code_operator_less_than_or_equal_to():
+        actions.auto_insert(" <= ")
+
+    def code_operator_and():
+        actions.auto_insert(" and ")
+
+    def code_operator_or():
+        actions.auto_insert(" or ")
+
+    def code_operator_in():
+        actions.auto_insert(" in ")
+
+    def code_operator_not_in():
+        actions.auto_insert(" not in ")
+
+    def code_operator_bitwise_and():
+        actions.auto_insert(" & ")
+
+    def code_operator_bitwise_and_assignment():
+        actions.auto_insert(" &= ")
+
+    def code_operator_bitwise_or():
+        actions.auto_insert(" | ")
+
+    def code_operator_bitwise_or_assignment():
+        actions.auto_insert(" |= ")
+
+    def code_operator_bitwise_exclusive_or():
+        actions.auto_insert(" ^ ")
+
+    def code_operator_bitwise_exclusive_or_assignment():
+        actions.auto_insert(" ^= ")
+
+    def code_operator_bitwise_left_shift():
+        actions.auto_insert(" << ")
+
+    def code_operator_bitwise_left_shift_assignment():
+        actions.auto_insert(" <<= ")
+
+    def code_operator_bitwise_right_shift():
+        actions.auto_insert(" >> ")
+
+    def code_operator_bitwise_right_shift_assignment():
+        actions.auto_insert(" >>= ")
+
+    def code_self():
+        actions.auto_insert("self")
+
+    def code_operator_object_accessor():
+        actions.auto_insert(".")
+
+    def code_insert_null():
+        actions.auto_insert("null")
+
+    # def code_insert_is_null():
+    #     actions.auto_insert(" is None")
+
+    # def code_insert_is_not_null():
+    #     actions.auto_insert(" is not None")
+
+    def code_state_if():
+        actions.user.insert_between("if ", ":")
+
+    def code_state_else_if():
+        actions.user.insert_between("elif ", ":")
+
+    def code_state_else():
+        actions.insert("else:")
+        actions.key("enter")
+
+    def code_state_switch():
+        actions.user.insert_between("match ", ":")
+
+    def code_state_case():
+        actions.insert(":")
+        actions.edit.left()
+
+    def code_state_for():
+        actions.auto_insert("for ")
+
+    def code_state_for_each():
+        actions.user.insert_between("for ", " in ")
+
+    def code_state_while():
+        actions.user.insert_between("while ", ":")
+
+    def code_define_class():
+        actions.auto_insert("class ")
+
+    # def code_import():
+    #     actions.auto_insert("import ")
+
+    def code_comment_line_prefix():
+        actions.auto_insert("# ")
+
+    def code_state_return():
+        actions.insert("return ")
+
+    def code_insert_true():
+        actions.auto_insert("true")
+
+    def code_insert_false():
+        actions.auto_insert("false")
+
+    def code_comment_documentation():
+        actions.auto_insert("## ")
+
+    def code_insert_function(text: str, selection: str):
+        text += f"({selection or ''})"
+        actions.user.paste(text)
+        actions.edit.left()
+
+    def code_default_function(text: str):
+        actions.user.code_public_function(text)
+
+    # def code_private_function(text: str):
+    #     """Inserts private function declaration"""
+    #     result = "def _{}():".format(
+    #         actions.user.formatted_text(
+    #             text, settings.get("user.code_private_function_formatter")
+    #         )
+    #     )
+
+    # actions.user.paste(result)
+    # actions.edit.left()
+    # actions.edit.left()
+
+    def code_public_function(text: str):
+        result = "func {}():".format(
+            actions.user.formatted_text(
+                text, settings.get("user.code_public_function_formatter")
+            )
+        )
+        actions.user.paste(result)
+        actions.edit.left()
+        actions.edit.left()
+
+    def code_insert_type_annotation(type: str):
+        actions.insert(f": {type}")
+
+    def code_insert_return_type(type: str):
+        actions.insert(f" -> {type}")
