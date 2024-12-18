c7dce8c10fce0dd386a0f403656647bee99af110
narjoDev <narjoDev@users.noreply.github.com>
2024-12-18 11:44:37 -0800

    add coderpad with some editing commands

---
+++ apps/coderpad/coderpad.py
@@ -0,0 +1,30 @@
+from talon import Context, Module, actions
+
+mod = Module()
+ctx = Context()
+
+apps = mod.apps
+apps.coderpad = r"""
+tag: browser
+browser.host: app.coderpad.io
+"""
+
+ctx.matches = r"""
+app: coderpad
+"""
+
+ctx.tags = ["user.line_commands"]
+
+
+@ctx.action_class("edit")
+class EditActions:
+    def jump_line(n: int):
+        actions.key("ctrl-g")
+        actions.insert(str(n))
+        actions.key("enter")
+
+    def indent_more():
+        actions.key("ctrl-]")
+
+    def indent_less():
+        actions.key("shift-tab")
---
+++ apps/coderpad/coderpad.talon
@@ -0,0 +1,8 @@
+app: coderpad
+-
+trim trail:
+    key(ctrl-k)
+    key(ctrl-x)
+
+cursor up: key("ctrl-alt-up")
+cursor down: key("ctrl-alt-down")
